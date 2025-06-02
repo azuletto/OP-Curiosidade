40244b4af053db8c3518133692f12feaba9529d3
azuletto <az.oliveira.andre@gmail.com>
2025-06-02 15:02:16 -0300

    #96273 - criação do modal

---
+++ cadastro-page/script/modal.js
@@ -0,0 +1,11 @@
+const modalButton = document.getElementById('register-button');
+const modalClose = document.getElementById('exit-register-modal');
+const modal = document.querySelector('dialog');
+
+modalButton.onclick = function () {
+    modal.showModal()
+}
+
+modalClose.onclick = function () {
+    modal.close()
+}
