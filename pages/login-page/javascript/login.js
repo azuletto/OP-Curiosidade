c413710131751223004a0f84bfe74784f6f70f02
André Malsemiro da Silva de Oliveira <andre.oliveira@forlogic.net>
2025-06-22 18:55:11 -0300

    #96581 - mostrar/ocultar senha no login

--- pages/login-page/javascript/login.js
+++ pages/login-page/javascript/login.js
@@ -3,15 +3,20 @@ import { regexEmail } from "/global/src/javascripts/email-regex.js";
 const login_button = document.getElementById("login-button");
 const error_message = document.getElementById("error-message");
 const login_error_message = document.getElementById("login-error-message")
+const show_pass_btn = document.getElementById("show-pass-button")
+const pass_input = document.getElementById("password")
 const urlParams = new URLSearchParams(window.location.search);
 const errorParam = urlParams.get("error");
+
+let showPass;
+
 if (errorParam === "not_logged") {
     login_error_message.innerHTML = "Você <strong>não está logado</strong>, entre com um usuário para continuar.";
 }
 const user = {
     username: "admin",
     password: "opcuriosidade",
-    email: "admin@admin"
+    email: "admin@curiosidade.com"
 }
 
 login_button.addEventListener("click", function () {
@@ -32,6 +37,17 @@ login_button.addEventListener("click", function () {
         }
     } else {
         error_message.innerHTML = "Você inseriu um e-mail inválido. Tente novamente."
+    }
+})
+
+show_pass_btn.addEventListener("click",()=> {
+    event.preventDefault()
+    if(!showPass){showPass = true
+        pass_input.type = "text"
+        show_pass_btn.style.background = "var(--show-button-url)"
+    }else if(showPass){showPass=false
+        pass_input.type = "password"
+        show_pass_btn.style.background = "var(--hide-button-url)"
     }
 })
 
