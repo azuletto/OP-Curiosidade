b6b4bec875b60b38b87c70ab425202b133641113
André Oliveira <78551538+azuletto@users.noreply.github.com>
2025-07-14 08:53:13 -0300

    #99611 - correção da bateria de testes (RegeX com falha) + novos testes para nome de usuário

--- api/Tests/AdminTest.cs
+++ api/Tests/AdminTest.cs
@@ -8,8 +8,27 @@ public sealed class AdminTest
     [TestMethod]
     public void Admin_Name_Not_Valid_By_Widht()
     {
-        Admin admin = new Admin(name:"Ricardo",email:"admin@email.com",isDeleted: false,password:"ASDassd546#");
-        Console.WriteLine(admin.Name);
+        Admin admin = new Admin(name: "", email: "admin@email.com", isDeleted: false, password: "ASDassd546#");
+        Assert.AreEqual(true, admin.Validation());
+    }
+
+    [TestMethod]
+    public void Admin_Name_Not_Valid_By_Characters()
+    {
+        Admin admin = new Admin(name: "Admin@123", email: "adim@email.com", isDeleted: false, password: "ASDassd546#");
+        Assert.AreEqual(true, admin.Validation());
+    }
+    [TestMethod]
+    public void Admin_Name_Imput_Not_Valid_By_Sql_Injection()
+    {
+        Admin admin = new Admin(name: "Admin' OR 1=1; --", email: "admin@email.com", isDeleted: false, password: "ASDassd546#");
+        Assert.AreEqual(true, admin.Validation());
+
+    }
+    [TestMethod]
+    public void Admin_Name_Is_Valid()
+    {
+        Admin admin = new Admin(name: "Ricardo Gonçalves Filho", email: "admin@email.com", isDeleted: false, password: "ASDassd546#");
         Assert.AreEqual(true, admin.Validation());
     }
 }
